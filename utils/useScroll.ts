0eb49c16f2149b1d13b8cab39d1e2011a4813ab1
Omar Diop <accounts@omardiop.com>
2023-03-11 16:59:31 +0100

    Fixed header scroll animation

--- utils/useScroll.ts
+++ utils/useScroll.ts
@@ -2,21 +2,40 @@ import { useState, useEffect } from "react"
 
 type ScrollDirection = "up" | "down"
 
+const SCROLL_THRESHOLD = 10
+const SCROLL_TO_TOP_THRESHOLD = 50
+
 const useScroll = () => {
   const [direction, setScrollDirection] = useState<ScrollDirection>("down")
   const [scrolledToTop, setScrolledToTop] = useState(true)
 
   useEffect(() => {
     let lastScrollY = window.pageYOffset
+    let scrolling = false
 
-    const onScroll = () => {
+    const updateScroll = () => {
       const scrollY = window.pageYOffset
       const currentDirection = scrollY > lastScrollY ? "down" : "up"
+
+      //Animate only if user has scrolled a threshold amount of pixels
+      if (Math.abs(scrollY - lastScrollY) < SCROLL_THRESHOLD) {
+        scrolling = false
+        return
+      }
+
       if (currentDirection !== direction) {
         setScrollDirection(currentDirection)
       }
       lastScrollY = scrollY > 0 ? scrollY : 0
-      setScrolledToTop(scrollY < 50)
+      scrolling = false
+      setScrolledToTop(scrollY < SCROLL_TO_TOP_THRESHOLD)
+    }
+
+    const onScroll = () => {
+      if (!scrolling) {
+        scrolling = true
+        window.requestAnimationFrame(updateScroll)
+      }
     }
     window.addEventListener("scroll", onScroll)
     return () => {

6c2b96f457f779b2eec4d4b5bc0749dfd5892d67
Omar Diop <accounts@omardiop.com>
2024-09-26 10:50:09 +0200

    fix: seo canonical url and description

--- components/Seo/Seo.tsx
+++ components/Seo/Seo.tsx
@@ -4,20 +4,20 @@ export function Seo() {
   return (
     <DefaultSeo
       title="Omar Diop | Software Engineer"
-      description="Sono un appassionato sviluppatore con esperienza nello sviluppo di software, applicazioni, API e architetture software, unisco competenze tecniche e creative in ogni progetto. La mia esperienza in UI/UX, funnel marketing e prodotto mi consente di essere un professionista digitale completo. Il mio obiettivo è quello di crescere costantemente condividendo tutto ciò che imparo lungo il percorso."
-      canonical="https://omardiop.com"
+      description="Passionate Software Engineer and Technical Lead at Learnn with expertise in product development, including UX/UI design, product management, and marketing. Dedicated to becoming a well-rounded digital professional, sharing insights and knowledge to help others grow in their tech careers."
+      canonical="https://www.omardiop.com"
       openGraph={{
         type: "website",
         locale: "it_IT",
-        url: "https://omardiop.com",
-        site_name: "Omar Diop | Software Engineer",
-        title: "Omar Diop | Software Engineer",
+        url: "https://www.omardiop.com",
+        site_name: "Omar Diop | Software Engineer, International Speaker",
+        title: "Omar Diop | Software Engineer, International Speaker",
         description:
-          "Sono un appassionato sviluppatore con esperienza nello sviluppo di software, applicazioni, API e architetture software, unisco competenze tecniche e creative in ogni progetto. La mia esperienza in UI/UX, funnel marketing e prodotto mi consente di essere un professionista digitale completo. Il mio obiettivo è quello di crescere costantemente condividendo tutto ciò che imparo lungo il percorso.",
+          "Passionate Software Engineer and Technical Lead at Learnn with expertise in product development, including UX/UI design, product management, and marketing. Dedicated to becoming a well-rounded digital professional, sharing insights and knowledge to help others grow in their tech careers.",
         images: [
           {
-            url: "https://omardiop.com/images/banner.jpg",
-            alt: "Omar Diop | Software Engineer , Conference Speaker",
+            url: "https://www.omardiop.com/images/banner.jpg",
+            alt: "Omar Diop | Software Engineer , International Speaker",
             height: 1200,
             width: 630,
             type: "image/png",

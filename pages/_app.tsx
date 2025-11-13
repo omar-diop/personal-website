de08f53f1d21e7043e8b2c6f832d9bd27fc82d8a
Omar Diop <accounts@omardiop.com>
2025-11-13 19:19:36 +0100

    feat: added posthog and reverse proxy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/typography.css"
 import "../styles/globals.css"
 import * as style from "../styles/globals.css"
-
+import { useEffect } from "react"
 import type { AppProps } from "next/app"
 
 import { NavBar } from "../components/NavBar"
@@ -9,10 +9,25 @@ import { Footer } from "../components/Footer"
 import { Socials } from "../components/Socials"
 import { Seo } from "../components/Seo"
 import { TagManager } from "../components/TagManager"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: "/relay-4mii/",
+      ui_host: "https://eu.posthog.com",
+      person_profiles: "identified_only",
+      defaults: "2025-05-24",
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug()
+      },
+    })
+  }, [])
+
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <TagManager />
       <div className={style.container}>
         <Seo />
@@ -25,6 +40,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Socials viewport="desktop" />
         <Footer />
       </div>
-    </>
+    </PostHogProvider>
   )
 }
